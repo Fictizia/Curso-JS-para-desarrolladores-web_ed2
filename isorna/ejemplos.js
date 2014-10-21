65dc03a927faf1e95e6767a42c0039ea07664cff
Alvaro Isorna <alvaro.isorna@gmail.com>
2014-10-21 09:56:21 +0000

    carpeta de Alvaro

---
+++ isorna/ejemplos.js
@@ -0,0 +1,19 @@
+var ghTablaHash = {
+    clave1: 'valor1'
+};
+
+ghTablaHash.clave2 = 'valor 2';
+
+console.log(ghTablaHash.clave1, ghTablaHash.clave2);
+
+var calcularIRPF = function () {
+    
+};
+
+function calcularIVA (piTipo, pcNombre) {
+    console.log(arguments);
+};
+
+calcularIVA(16, 'libros', 5, 'medicinas');
+
+//$('.input').on('click', function input_onClick(){/**/});
