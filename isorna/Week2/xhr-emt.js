0f57a2223967c51c8a7f6a24dadf945bb05824fb
Alvaro Isorna <alvaro.isorna@gmail.com>
2014-11-03 13:04:27 +0000

    Week 3

--- isorna/Week2/xhr-emt.js
+++ isorna/Week2/xhr-emt.js
@@ -3,7 +3,8 @@
 var xmlHttp = new XMLHttpRequest(),
     cURL = 'https://openbus.emtmadrid.es:9443/emt-proxy-server/last/bus/GetRouteLines.php',
     cIdClient = 'WEB.SERV.alvaro@isorna.net',
-    cPassKey = '075CF186-4345-4F0C-8604-C22BE13562D4';
+    cPassKey = '075CF186-4345-4F0C-8604-C22BE13562D4',
+    cParams = 'SelectDate=30/10/2014&Lines=145&idClient=' + cIdClient + '&PassKey=' + cPassKey;
 
 xmlHttp.onreadystatechange = function () {
     console.log(xmlHttp, 'ReadyState: ' + xmlHttp.readyState, ' Status: ' + xmlHttp.status);
@@ -15,4 +16,60 @@ xmlHttp.onreadystatechange = function () {
 
 xmlHttp.open( "POST", cURL, true );
 xmlHttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-xmlHttp.send('SelectDate=30/10/2014&Lines=145&idClient=' + cIdClient + '&PassKey=' + cPassKey);
+xmlHttp.setRequestHeader("Content-length", cParams.length);
+xmlHttp.setRequestHeader("Connection", "close");
+
+xmlHttp.send(cParams);
+
+// CORS Request
+
+// Create the XHR object.
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+    // XHR for Chrome/Firefox/Opera/Safari.
+    xhr.open(method, url, true);
+  } else if (typeof XDomainRequest != "undefined") {
+    // XDomainRequest for IE.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+  } else {
+    // CORS not supported.
+    xhr = null;
+  }
+  return xhr;
+}
+
+// Make the actual CORS request.
+function makeCorsRequest() {
+  // All HTML5 Rocks properties support CORS.
+  var cURL = 'https://openbus.emtmadrid.es:9443/emt-proxy-server/last/bus/GetRouteLines.php',
+    cIdClient = 'WEB.SERV.alvaro@isorna.net',
+    cPassKey = '075CF186-4345-4F0C-8604-C22BE13562D4',
+    cParams = 'SelectDate=30/10/2014&Lines=145&idClient=' + cIdClient + '&PassKey=' + cPassKey;
+
+  var xhr = createCORSRequest('POST', cURL);
+  if (!xhr) {
+    alert('CORS not supported');
+    return;
+  }
+
+  // Response handlers.
+  xhr.onload = function() {
+    console.log('CORS XHR', xhr.responseText);
+  };
+
+  xhr.onerror = function() {
+    alert('Woops, there was an error making the request.');
+  };
+  
+  xhr.onreadystatechange = function () {
+    console.log(xmlHttp, 'ReadyState: ' + xmlHttp.readyState, ' Status: ' + xmlHttp.status);
+    
+    if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+        console.log(xmlHttp.responseText);
+    }
+  };
+
+  xhr.send(cParams);
+}
--- isorna/Week3/Week3.md
+++ isorna/Week3/Week3.md
@@ -1,16 +1,44 @@
 Week 3 Milestone
 ===================
 
-* Object-oriented Programming
-* Prototypes (revisited)
-* Constructors
-* Object hasOwnProperty & propertyIsEnumerable
-* Bind, Call & Apply
+__JSON & JSONP__
 
-* In-depth explanation of objects:
- * Private properties vs public properties.
- * [How to simulate private properties](https://developer.mozilla.org/en-US/Add-ons/SDK/Guides/Contributor_s_Guide/Private_Properties).
- * [Introduction to Object-Oriented Programming](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript).
- * More info:
+* [JSON with padding](http://web.ontuts.com/tutoriales/jsonp-llamadas-ajax-entre-dominios/).
+
+__Error handling__
+
+* Take a look at `error-handling.js` for code explanation.
+* Don't use `try / catch` statements, due to memory.
+* Talk about preventive error handling with function patterns.
+
+__Object-oriented Programming__
+
+* In-depth explanation of objects.
+* Private properties vs public properties.
+* [How to simulate private properties](https://developer.mozilla.org/en-US/Add-ons/SDK/Guides/Contributor_s_Guide/Private_Properties).
+* [Introduction to Object-Oriented Programming](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript).
+* More info:
  * [Create an object oriented javascript class constructor](http://www.htmlgoodies.com/html5/tutorials/create-an-object-oriented-javascript-class-constructor.html).
  * [__ECMA 6__ Object prototype constructor](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/create).
+
+__Prototypes (revisited)__
+
+
+__Constructors__
+
+
+__Object hasOwnProperty & propertyIsEnumerable__
+
+
+__Bind, Call & Apply__
+
+* Review definition and uses.
+
+__Practice__
+
+* Use facebook login for our backbone app.
+ * __Change the link to the latest version of firebase:__ `//cdn.firebase.com/js/client/1.1.3/firebase.js`.
+ * [Setup callback url for your facebook app](https://www.firebase.com/docs/web/guide/login/facebook.html).
+ * [Follow firebase login guidelines](https://www.firebase.com/docs/web/guide/user-auth.html).
+ * [Set up firebase security rules](https://www.firebase.com/docs/security/guide/user-security.html).
+* Use [Open Data EMT](http://opendata.emtmadrid.es/Servicios-web/BUS) for reading bus lines' information.
