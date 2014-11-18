541f980504983e97b47c01d58d5c0ce41b02db24
isaacsc <isaacsc@hotmail.com>
2014-11-18 10:17:53 +0000

    ejemplo app

---
+++ Isaac/fictizia/js/app/main.js
@@ -0,0 +1,6 @@
+define(["json2", "jquery", "underscore", "firebase", "backbone", "bootstrap", "backfire", "utils", "transport"], function($) {
+    // document.ready
+    // $(function() {
+    //     $('body').alpha().beta();
+    // });
+});
