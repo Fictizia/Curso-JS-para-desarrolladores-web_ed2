60533b41c76a5bf363cead1bb3efc99d3d33cef1
Alvaro Isorna <alvaro.isorna@gmail.com>
2014-11-18 10:09:16 +0000

    Week 5

--- isorna/Week5/library.js
+++ isorna/Week5/library.js
@@ -1,3 +1,4 @@
+/* globals: Firebase */
 // Vehicle Constructor
 function Vehicle (pHeading, pId, pRouteTag, pVtype) {
     var dNow = new Date();
@@ -40,4 +41,69 @@ Route.prototype.updateFromDB = function (pVehicles) {
         newVehicle = new Vehicle(pVehicle.heading, pVehicle.id, pVehicle.routeTag, pVehicle.vtype);
         pVehicles.push(newVehicle);
     }
-}
+};
+
+// DB Configuration
+var gcDB_URL = 'https://js-web-developers.firebaseio.com';
+
+// Singleton Module
+var myApp = (function (Firebase, pcDB_URL) {
+    var oInstance = {};
+    
+    function initModule () {
+        var cDB_URL = pcDB_URL;
+        
+        function db_connect () {
+            // https://www.firebase.com/docs/web/guide/user-auth.html
+            this.oRef = new Firebase(cDB_URL);
+        }
+        
+        function db_auth (pcEmail, pcPassword) {
+            // https://www.firebase.com/docs/web/guide/login/password.html
+            this.oRef.authWithPassword({
+                email    : pcEmail,
+                password : pcPassword
+            }, function(error, authData) {
+                if (error === null) {
+                    console.log("User ID: " + authData.uid + ", Provider: " + authData.provider);
+                } else {
+                    console.log("Error authenticating user:", error);
+                }
+                console.log(authData);
+            }, {
+                remember: "sessionOnly"
+            });
+        };
+        
+        function db_loadRoutes () {
+            // ...
+        };
+        
+        function db_loadVehicles () {
+            // ...
+        };
+        
+        function app_initialize () {
+            // ...
+            console.log('App initialized');
+        };
+        
+        return {
+            connect: db_connect,
+            auth: db_auth,
+            initialize: app_initialize
+        };
+    }
+    
+    return {
+        getInstance: function () {
+            if (!oInstance) {
+                oInstance = initModule();
+            }
+            
+            return oInstance;
+        }
+    };
+})(Firebase, gcDB_URL);
+
+myApp.initialize();
