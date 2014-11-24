62e463a72c26d8363c9c99f72a9665d1becfd2f0
Alvaro Isorna <alvaro.isorna@gmail.com>
2014-11-24 18:11:47 +0000

    Week 6

--- isorna/Week6/app/js/app.js
+++ isorna/Week6/app/js/app.js
@@ -1,66 +1,57 @@
-/* globals: Firebase */
+/* globals: Firebase, requirejs */
 
-// DB Configuration
-var gcDB_URL = 'https://js-web-developers.firebaseio.com';
+// For any third party dependencies, like jQuery, place them in the lib folder.
 
-// Singleton Module
-var myApp = (function (Firebase, pcDB_URL) {
-    var oInstance = {};
-    
-    function initModule () {
-        var cDB_URL = pcDB_URL;
-        
-        function db_connect () {
-            // https://www.firebase.com/docs/web/guide/user-auth.html
-            this.oRef = new Firebase(cDB_URL);
+// Configure loading modules from the lib directory,
+// except for 'app' ones, which are in a sibling
+// directory.
+requirejs.config({
+    baseUrl: 'js/lib',
+    paths: {
+        app: '../app',
+        firebase: 'firebase'
+    },
+    shim: {
+        'firebase': {
+            exports: 'Firebase'
         }
-        
-        function db_auth (pcEmail, pcPassword) {
-            // https://www.firebase.com/docs/web/guide/login/password.html
-            this.oRef.authWithPassword({
-                email    : pcEmail,
-                password : pcPassword
-            }, function(error, authData) {
-                if (error === null) {
-                    console.log("User ID: " + authData.uid + ", Provider: " + authData.provider);
-                } else {
-                    console.log("Error authenticating user:", error);
-                }
-                console.log(authData);
-            }, {
-                remember: "sessionOnly"
-            });
-        };
-        
-        function db_loadRoutes () {
-            // ...
-        };
-        
-        function db_loadVehicles () {
-            // ...
-        };
-        
-        function app_initialize () {
-            // ...
-            console.log('App initialized');
-        };
-        
-        return {
-            connect: db_connect,
-            auth: db_auth,
-            initialize: app_initialize
-        };
     }
-    
-    return {
-        getInstance: function () {
-            if (!oInstance) {
-                oInstance = initModule();
-            }
-            
-            return oInstance;
-        }
-    };
-})(Firebase, gcDB_URL);
+});
 
-myApp.initialize();
+// Start loading the main app file. Put all of
+// your application logic in there.
+requirejs(['classes', 'db'], function (classes, db) {
+    var cDB_URL = 'https://js-web-developers.firebaseio.com',
+        cDB_TRANSIT_URL = 'https://publicdata-transit.firebaseio.com/sf-muni',
+        oMyAppDB = {},
+        oTransitDB = {};
+    //Firebase and the classes module are all
+    //loaded and can be used here now.
+    
+    console.log('loaded!');
+    
+    oMyAppDB = db.newDB(cDB_URL);
+    
+    document.querySelector('#loginUser').addEventListener('click', function loginUser_onClick (event) {
+        oMyAppDB.auth(document.querySelector('#userEmail').value, document.querySelector('#userPwd').value);
+        
+        if (oMyAppDB.getAuth()) {
+            document.querySelector('#loginUser').className = 'hidden';
+            document.querySelector('#logoutUser').className = '';
+        }
+    });
+    
+    document.querySelector('#logoutUser').addEventListener('click', function logoutUser_onClick (event) {
+        oMyAppDB.unauth();
+        document.querySelector('#loginUser').className = '';
+        document.querySelector('#logoutUser').className = 'hidden';
+    });
+    
+//         function db_loadRoutes () {
+//             // ...
+//         };
+        
+//         function db_loadVehicles () {
+//             // ...
+//         };
+});
---
+++ isorna/Week6/app/js/lib/db.js
@@ -0,0 +1,76 @@
+// Classes Module
+define(['firebase'], function (Firebase) {
+    var newDB = function (pcDB_URL, poFunctions) {
+        var cache = {};
+            
+        cache._onValue = function (poSnapshot) {
+            console.log(poSnapshot.val());
+        };
+        
+        cache._onError = function (poErrorObject) {
+            console.log("The read failed: " + poErrorObject.code);
+        };
+        
+        cache._onAuth = function (poError, poAuthData) {
+            if (poError === null) {
+                console.log("User ID: " + poAuthData.uid + ", Provider: " + poAuthData.provider);
+            } else {
+                console.log("Error authenticating user:", poError);
+            }
+            console.log(poAuthData);
+        };
+        
+        cache._connect = function () {
+            cache.oRef = new Firebase(cache.cDB_URL);
+            
+            cache.oRef.once('value', cache._onValue, cache._onError);
+        };
+        
+        cache._disconnect = function () {
+            cache.oRef.off();
+        };
+        
+        // https://www.firebase.com/docs/web/guide/login/password.html
+        cache._auth = function (pcEmail, pcPassword) {
+            cache.oRef.authWithPassword({
+                email    : pcEmail,
+                password : pcPassword
+            }, cache._onAuth, {
+                remember: "sessionOnly"
+            });
+        };
+        
+        cache._unauth = function () {
+            cache.oRef.unauth();
+        };
+        
+        cache._getAuth = function () {
+            return cache.oRef.getAuth();
+        };
+        
+        cache._initialize = function (pcDB_URL, poFunctions) {
+            cache.cDB_URL = pcDB_URL;
+            
+            if (typeof poFunctions != 'undefined') {
+                cache._onValue = poFunctions.onValue || cache._onValue;
+                cache._onError = poFunctions.onError || cache._onError;
+                cache._onAuth = poFunctions.onAuth || cache._onAuth;
+            }
+        };
+        
+        cache._initialize(pcDB_URL, poFunctions);
+        cache._connect();
+    
+        return {
+            connect: cache._connect,
+            disconnect: cache._disconnect,
+            auth: cache._auth,
+            unauth: cache._unauth,
+            getAuth: cache._getAuth
+        };
+    };
+    
+    return {
+        newDB: newDB
+    };
+});
