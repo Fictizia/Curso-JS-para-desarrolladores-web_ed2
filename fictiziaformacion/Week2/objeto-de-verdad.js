01e378c5af6eeb5acd08644204658df3fc4f14d7
Fictizia <direccion@fictizia.com>
2015-01-31 11:28:33 +0000

    Week 2

--- fictiziaformacion/Week2/objeto-de-verdad.js
+++ fictiziaformacion/Week2/objeto-de-verdad.js
@@ -1,6 +1,15 @@
 'use strict';
 
-var Persona = function (pcNombre){
+// declaro mi app
+
+var miApp = {};
+
+// declaro el namespace de clases
+
+miApp.clases = {};
+
+// 1. patron de modulos con funciones en memoria
+miApp.clases.Persona = function (pcNombre){
     var personita = {};
     
     personita.nombre = pcNombre;
@@ -19,3 +28,36 @@ function andar (piMetros) {
 function saludar () {
     console.log('Hola!, me llamo ' + this.nombre);
 }
+
+// 2. patron de constructor de clase
+miApp.clases.Persona2 = function (pcNombre){
+    var oPersonita = {},
+        _cNombre = pcNombre;
+    
+    oPersonita.seMueve = _fAndar;
+    oPersonita.saluda = _fSaludar;
+    oPersonita.getNombre = _getNombre;
+    oPersonita.setNombre = _setNombre;
+    
+    return oPersonita;
+    
+    /* libreria de funciones */
+    
+    function _getNombre () {
+        return _cNombre;
+    }
+    
+    function _setNombre (pcNuevoNombre) {
+        _cNombre = pcNuevoNombre;
+    }
+
+    function _fAndar (piMetros) {
+        console.info('Ando ' + piMetros + ' metros');
+    }
+    
+    function _fSaludar () {
+        console.log('Hola!, me llamo ' + _getNombre());
+    }
+};
+
+var miYo = miApp.clases.Persona2('Alvaro');
