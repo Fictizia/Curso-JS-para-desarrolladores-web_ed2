cc10fa2e55ea8e592c4c50386b66e77cd82b1a78
cpoison <c.poison8@gmail.com>
2015-02-14 14:00:04 +0000

    semana 4-2

--- cpoison/html5-boilerplate-4.3.0/js/main.js
+++ cpoison/html5-boilerplate-4.3.0/js/main.js
@@ -1,1 +1,13 @@
+'use strict';
 
+var myApp = {};
+
+myApp.persona = {};
+
+myApp.persona.alumnos = function(){
+    var alumno = {};
+    alumno.nombre = "";
+    alumno.apellido = "";
+    
+    return alumno;
+}
