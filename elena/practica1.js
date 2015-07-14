07355ee39b11a01492965838e0643912f86f5197
emadrigal <elenayuscula@gmail.com>
2015-07-14 09:42:33 +0000

    practica clase 2

--- elena/practica1.js
+++ elena/practica1.js
@@ -1,6 +1,37 @@
-var miLista = ["perro", "gato", "vaca"];
+/*var miLista = ["perro", "gato", "vaca"];
 
 for (var i = 0; i<3; i++) {
 var mensaje = miLista[i];
 console.log(mensaje);
+}
+
+
+
+
+
+function escribirLista() {
+    console.log(mensaje)
+}
+
+
+for (var i = 0; i<=3; i++) {
+var mensaje = miLista[i];
+escribirLista();
+} */
+
+
+
+var miLista = ["perro", "gato", "vaca"],
+    i;
+
+
+for (i = 0; i<3; i++) {
+var mensaje = miLista[i];
+ console.log(mensaje)
+}
+
+
+
+function escribirLista() {
+   
 }
